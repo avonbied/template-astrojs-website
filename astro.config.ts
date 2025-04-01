3c694da60ee45def3c8c3bc2a8e19c9bb6fa335e
Lex <58815784+avonbied@users.noreply.github.com>
2025-03-31 23:45:37 -0500

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import pwa from '@vite-pwa/astro';
 
 // https://astro.build/config
 export default defineConfig({
-	site: process.env.SITE_URL,
-	integrations: [sitemap()],
+	site: process.env.SITE_URL ?? 'localhost',
+	base: process.env.SITE_BASE,
+	integrations: [sitemap(), pwa()],
 	server: {
 		port: Number(process.env.PORT ?? 8080),
 		host: true,
